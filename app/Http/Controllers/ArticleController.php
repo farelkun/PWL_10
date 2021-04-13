40c982230eb8642f1821e65f2071010ae5b6cae1
fxrel <farelhidayat123@gmail.com>
2021-04-14 02:25:28 +0700

    membuat store pada controller article dan storege

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -24,7 +24,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('articles.create');
     }
 
     /**
@@ -35,7 +35,17 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if($request->file('image')) {
+            $image_name = $request->file('image')->store('images', 'public');
+        }
+
+        Article::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'feature_image' => $image_name,
+        ]);
+
+        return redirect()->route('articles.index')->with('success', 'Data saved successfully');
     }
 
     /**
