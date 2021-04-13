b9bb716e6396d60e6be607fbdd26fd3416be7c99
fxrel <farelhidayat123@gmail.com>
2021-04-14 02:49:33 +0700

    membuat file articles_pdf di folder articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,8 +15,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::all();
-        return view('articles.index', compact('articles'));
+        //
     }
 
     /**
